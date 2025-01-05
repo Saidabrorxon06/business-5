7c584a2fdf4788c47f824acdb5ea8ff9fe250239
Saidabrorxon06 <saidabrorahmadov@icloud.com>
2025-01-05 06:28:33 +0500

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://h9hYHIz-3GCgk5F7iYm2p9lrcpUFMAUI_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AH4-Business planning"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
